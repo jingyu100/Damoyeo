1a86548a7f5476b45a5b9e881de5ce4cd2f150cb
jingyu100 <jingyu100@naver.com>
2024-11-01 14:27:57 +0900

    스프링부트 설치

---
+++ Damoyeo/src/main/java/Team6/Damoyeo/DamoyeoApplication.java
@@ -0,0 +1,13 @@
+package Team6.Damoyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamoyeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamoyeoApplication.class, args);
+	}
+
+}
---
+++ Damoyeo/src/test/java/Team6/Damoyeo/DamoyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package Team6.Damoyeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamoyeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
