9772a826b756e5839d8d7a9a45cbd068e8cac305
jingyu100 <jingyu100@naver.com>
2024-11-04 22:46:11 +0900

    Add Post Detail href

--- Damoyeo/src/main/java/Team6/Damoyeo/Post/Controller/PostController.java
+++ Damoyeo/src/main/java/Team6/Damoyeo/Post/Controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -40,5 +41,10 @@ public class PostController {
     public String createPost(Model model) {
         model.addAttribute("post", new Post());
         return "post/create";
+    }
+
+    @GetMapping("/detail{id}")
+    public String detailPost(Model model, @PathVariable int id) {
+        return "post/detail";
     }
 }
