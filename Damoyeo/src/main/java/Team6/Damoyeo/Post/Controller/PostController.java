e6dab75d1872bb007b0e50de5972b7611bcb902e
MoonJihoon5788 <munjihun3824@naver.com>
2024-11-05 20:51:58 +0900

    게시글사진업로드

--- Damoyeo/src/main/java/Team6/Damoyeo/Post/Controller/PostController.java
+++ Damoyeo/src/main/java/Team6/Damoyeo/Post/Controller/PostController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,7 +25,7 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
-
+    private static final String UPLOAD_DIRECTORY = "C:/uploads/";
     // 메인 화면
     @GetMapping("/main")
     public String showMainPage(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
@@ -52,8 +56,33 @@ public class PostController {
     }
     // 아직 사용자의 id 값 가져오는건 구현 안했음 ㅈㅅ
     @PostMapping("/create")
-    public String savePost(@ModelAttribute("post") Post post) {
+    public String savePost(@ModelAttribute("post") Post post,@RequestParam("photo") MultipartFile file,
+                           RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload.");
+            return "redirect:/";
+        }
+
+        try {
+            // 저장할 파일 경로 설정
+            String filePath = UPLOAD_DIRECTORY + file.getOriginalFilename();
+            File dest = new File(filePath);
+
+            // 파일 저장
+            file.transferTo(dest);
+
+            redirectAttributes.addFlashAttribute("message", "File uploaded successfully: " + file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("message", "File upload failed.");
+        }
+        post.setPhotoUrl(UPLOAD_DIRECTORY + file.getOriginalFilename());
         postService.savePost(post);
+
+
         return "redirect:/post/main";
     }
+
+
 }
